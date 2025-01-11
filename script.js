32ba304acf8b75b79136b23abff352265d149530
khabigcorp <99237998+khabigcorp@users.noreply.github.com>
2025-01-11 14:54:45 +0800

    Wrote code for computer choice in game

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+function getComputerChoice()
+{
+    let randomNumber = Math.floor(Math.random()*3);
+    if (randomNumber === 0)
+    {   
+        return "rock";
+    }
+    else if (randomNumber === 1)
+    {
+        return "paper";
+    }
+    else 
+    {
+        return "scissors";
+    }
+}
 console.log("Hello World");
